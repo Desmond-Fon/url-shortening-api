72957fbb1c16e87774904d01423cb3732d91d066
Desmond <desmondfon@yahoo.com>
2022-10-29 00:11:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      color: {
+        'cyan': 'hsl(180, 66%, 49%)',
+        'darkVIolet': 'hsl(257, 27%, 26%)',
+        'red': 'hsl(0, 87%, 67%)',
+        'grayy': 'hsl(0, 0%, 75%)',
+        'grayishViolet': 'hsl(257, 7%, 63%)',
+        'veryDarkBlue': 'hsl(255, 11%, 22%)',
+        'veryDarkViolet': 'hsl(260, 8%, 14%)'
+      }
+    },
+  },
+  plugins: [],
+}
